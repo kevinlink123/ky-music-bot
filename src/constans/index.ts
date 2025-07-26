45a36526e55c9f12bdada1b8cce9a221df2925a9
kevinlink123 <kevin_link_23@hotmail.com>
2025-07-25 21:08:57 -0300

    feat(app): Added constans and utils to extract functions and constans to its own modules

---
+++ src/constans/index.ts
@@ -0,0 +1,11 @@
+import path from "path";
+
+export const CONSTANS = {
+    MUSIC_DIR: path.join(__dirname, "..", "..", "music")
+}
+
+export const PLAY_MESSAGES = [
+    "AGARRATE EL ORTO QUE SUENA",
+    "QUE SE PREPAREN LOS OJETES QUE LLEGO EL MAS VERGA",
+    "UN MOGOLICO PUSO",
+]
