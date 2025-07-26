9ac94ea891f523561bd192b44f1cfa9ec62024e0
kevinlink123 <kevin_link_23@hotmail.com>
2025-07-26 19:53:21 -0300

    feat(app): Moved global types to root folder

---
+++ src/types.d.ts
@@ -0,0 +1,24 @@
+import { SlashCommandBuilder } from 'discord.js';
+import { AudioPlayer, VoiceConnection } from "@discordjs/voice";
+import { Channel, VoiceBasedChannel } from "discord.js";
+
+export type Command = {
+  data: SlashCommandBuilder;
+  execute: (interaction: ChatInputCommandInteraction) => Promise<void>;
+};
+
+export interface Queue {
+  textChannel: Channel;
+  voiceChannel: VoiceBasedChannel;
+  connection?: VoiceConnection;
+  songs: Song[];
+  player: AudioPlayer;
+  playing: boolean;
+}
+
+export interface Song {
+  title: string;
+  url: string;
+  duration: number;
+  local: boolean
+}
