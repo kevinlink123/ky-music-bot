7cf62a696b5168b7ef9c8679a6efad1d30be27c2
kevinlink123 <kevin_link_23@hotmail.com>
2025-07-26 20:39:51 -0300

    fix(commands): Fixed list-music command

--- src/commands/local-music/local.ts
+++ src/commands/local-music/local.ts
@@ -1,4 +1,4 @@
-import { SlashCommandBuilder } from 'discord.js';
+import { CacheType, ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
 import { CONSTANS } from '../../constans';
 import fs from 'fs';
 import { isMusicInFolder } from '../../utils/localHandler';
@@ -7,17 +7,20 @@ module.exports = {
 	data: new SlashCommandBuilder()
 	  .setName('list-music')
 		.setDescription('Muestra por mensajes las canciones disponibles'),
-	async execute(interaction: any) {
+	async execute(interaction: ChatInputCommandInteraction<CacheType>	) {
 		// interaction.guild is the object representing the Guild in which the command was run
-		if (isMusicInFolder()) {
+		if (!isMusicInFolder()) {
 			await interaction.reply("No descargaron nada todavia mogodowns");
 			return;	
 		}
-		
-		await interaction.reply("Estas son las canciones que tengo para reproducir:");
+
+		await interaction.deferReply();
+		const message = ['**Todas estas canciones tengo guardadas:**'];
 		const fileNames = fs.readdirSync(CONSTANS.MUSIC_DIR);
 		for (const file of fileNames) {
-			await interaction.reply(file);
+			message.push(file.slice(0, file.lastIndexOf(".")));
 		}
+
+		await interaction.editReply(message.join("\n"));
 	},
 };
