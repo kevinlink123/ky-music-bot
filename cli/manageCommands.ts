3641f494e3fb0770dd458f444effa5ecad03cde3
kevinlink123 <kevin_link_23@hotmail.com>
2025-07-28 01:33:42 -0300

    feat(cli): Added deploy:id command to deploy all commands to a server by its id

--- cli/manageCommands.ts
+++ cli/manageCommands.ts
@@ -1,6 +1,8 @@
 import { Command } from 'commander';
 import { config } from "dotenv";
 import { REST, Routes } from 'discord.js';
+import path from 'node:path';
+import fs from 'node:fs';
 config();
 
 const program = new Command();
@@ -34,6 +36,60 @@ async function deleteAllCommands() {
   }
 }
 
+async function deployCommandToServerById(serverId: string) {
+  if(!serverId) {
+    console.error("TENES QUE DARME EL ID DEL SERVIDOR AL QUE QUIERAS QUE REGISTRE LOS COMANDOS!");
+    return
+  }
+
+  const commands: { data: any, execute: () => any }[] = [];
+  // Grab all the command folders from the commands directory you created earlier
+  const foldersPath = path.join(__dirname, '..', 'src', 'commands');
+  const commandFolders = fs.readdirSync(foldersPath);
+  
+  for (const folder of commandFolders) {
+    // Grab all the command files from the commands directory you created earlier
+    const commandsPath = path.join(foldersPath, folder);
+    // Notice that here we are looking for .ts files (in the index we search for .js files 'cause after comp all files are .js)
+    // This is because we use ts-node to execute scripts 
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.ts'));
+    // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+    for (const file of commandFiles) {
+      const filePath = path.join(commandsPath, file);
+      const command: { data: any, execute: () => any } = require(filePath);
+      if ('data' in command && 'execute' in command) {
+        commands.push(command.data.toJSON());
+      } else {
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+      }
+    }
+  };
+
+  // Construct and prepare an instance of the REST module
+  // const rest = new REST().setToken(process.env.DS_TOKEN!);
+  
+  // and deploy your commands!
+  (async () => {
+    try {
+      const clientId = process.env.CLIENT_ID;
+      console.log(`Started refreshing ${commands.length} application (/) commands.`);
+  
+      // The put method is used to fully refresh all commands in the guild with the current set
+      const data = await rest.put(
+        Routes.applicationGuildCommands(clientId!, serverId!),
+        { body: commands },
+      );
+  
+      //@ts-ignore
+      console.log(`Successfully reloaded ${data.length} application (/) commands to server with id ${serverId}.`);
+    } catch (error) {
+      // And of course, make sure you catch and log any errors!
+      console.error(error);
+      
+    }
+  })();
+}
+
 program
   .name("manageCommands")
   .description("Herramienta de control de comandos")
@@ -57,4 +113,8 @@ program
   .description("Elimina un comando con el command id dado")
   .action(deleteCommandById)
 
+program
+  .command('deploy:id <serverId>')
+  .description('Registra todos los comandos creados a un server especifico')
+  .action(deployCommandToServerById)
 program.parse(process.argv);
