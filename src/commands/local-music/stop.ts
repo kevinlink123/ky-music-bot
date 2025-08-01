1c58bf505a11c0c14f295d77e6865f6692ed93ab
kevinlink123 <kevin_link_23@hotmail.com>
2025-08-01 18:21:30 -0300

    feat(stop): Added functionality to stop command

--- src/commands/local-music/stop.ts
+++ src/commands/local-music/stop.ts
@@ -1,3 +1,4 @@
+import { stopPlayer } from '../../utils/localHandler';
 import { ActivePlayer } from '../../types';
 import { SlashCommandBuilder, CacheType, ChatInputCommandInteraction } from 'discord.js';
 
@@ -5,8 +6,20 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName('stop')
     .setDescription('Detiene la cancion que este sonando'),
-  async execute(interaction: ChatInputCommandInteraction<CacheType>, activePlayer: ActivePlayer) {
-    console.log(activePlayer);
-    await interaction.reply("Comando en desarrollo...");
+  async execute(interaction: ChatInputCommandInteraction<CacheType>, activePlayers: Map<string, ActivePlayer>) {
+    const activePlayer = activePlayers.get(interaction.guildId!);
+    if(!activePlayer) {
+      await interaction.reply("Tomate tus pastillas flaco, no esta sonando nada en ningun lado");
+      return;
+    }
+
+    try {
+      stopPlayer(activePlayer);
+    } catch(err) {
+      console.error(err)
+      interaction.reply('Ocurrió un error al intentar frenar el player!');
+    }
+    activePlayer.player.stop();
+    await interaction.reply("Que lindo el silencio");
   }
 };
