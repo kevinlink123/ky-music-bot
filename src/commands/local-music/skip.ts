7707cdbf7e814135a4b6b21063ab7e5580c95db4
kevinlink123 <kevin_link_23@hotmail.com>
2025-08-08 18:55:54 -0300

    feat(skip): Added skip command logic

--- src/commands/local-music/skip.ts
+++ src/commands/local-music/skip.ts
@@ -1,10 +1,25 @@
+import { PLAY_MESSAGES } from '../../constans';
+import { ActivePlayer } from '../../services/ActivePlayer';
 import { SlashCommandBuilder, CacheType, ChatInputCommandInteraction } from 'discord.js';
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('skip')
     .setDescription('Skipea a la siguiente cancion'),
-  async execute(interaction: ChatInputCommandInteraction<CacheType>) {
-    await interaction.reply("Comando en desarrollo...");
+  async execute(interaction: ChatInputCommandInteraction<CacheType>, activePlayers: Map<string, ActivePlayer>) {
+    if (!interaction.guildId) return;
+    const currentActivePlayer = activePlayers.get(interaction.guildId);
+    if (!currentActivePlayer) {
+      await interaction.reply("No hay ningun player sonando esquizofrenico");
+      return;
+    }
+    if (!currentActivePlayer.getQueue().length) {
+      await interaction.reply("No hay nada en cola, pode a reproducir algo o ME ENOJO LOCO");
+      return;
+    }
+
+    currentActivePlayer.nextSong();
+    const replyMessage = PLAY_MESSAGES[Math.floor(Math.random() * PLAY_MESSAGES.length)];
+    await interaction.reply(`${replyMessage} ** ${currentActivePlayer.getCurrentTrack()?.title} **`);
   }
 };
