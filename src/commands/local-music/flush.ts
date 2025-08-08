58ee51ff7b4f1ee21903f9beda3ddb775ff1d394
kevinlink123 <kevin_link_23@hotmail.com>
2025-08-08 18:55:31 -0300

    feat(flush): Added logic for flush command with the commands pattern

--- src/commands/local-music/flush.ts
+++ src/commands/local-music/flush.ts
@@ -1,10 +1,23 @@
-import { SlashCommandBuilder, CacheType, ChatInputCommandInteraction } from 'discord.js';
+import { CONSTANS } from '../../constans';
+import { isMusicInFolder } from '../../utils/localHandler';
+import { SlashCommandBuilder, CacheType, ChatInputCommandInteraction, TextChannel } from 'discord.js';
+import path from 'node:path';
+import fs from 'node:fs';
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('flush')
     .setDescription('Elimina todas las canciones descargadas (NO LA USEN CADA DOS POR TRES MOGOLICOS)'),
   async execute(interaction: ChatInputCommandInteraction<CacheType>) {
-    await interaction.reply("Comando en desarrollo...");
+    if(!interaction.guildId) return;
+    const serverMusicFolder = path.join(CONSTANS.MUSIC_DIR, interaction.guildId);
+    
+    if (!isMusicInFolder(interaction.guildId)) {
+      interaction.reply("No me dieron nada para descargar todavia autistas");
+      return;
+    }
+    (interaction.channel as TextChannel).send("YA MISMO ELIMINO TODO LOCO, ME HINCHE LAS PELOTAS");
+    fs.rmSync(serverMusicFolder, { recursive: true, force: true });
+    await interaction.reply("Ahi termine, todo limpiecito");
   }
 };
